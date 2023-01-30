a7dd296830fc30a97e751a51f10bafbe0e0f5e54
Oliver Banks <oliver.banks@bbc.co.uk>
2023-01-30 14:49:01 +0000

    Week 2 progress

--- src/game.js
+++ src/game.js
@@ -1,11 +1,55 @@
 export const progressGame = (board) => {
-    return [[0,0,0],
-            [0,0,0],
-            [0,0,0]]; 
+  const nextTurn = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+
+  const firstRow = board[0];
+
+  for (let column of firstRow) {
+    if(board[0][column] === 1 && board[1][column] === 1 && board[2][column] === 1){
+        nextTurn[1][column] = 1;
+      }
+  }
+
+  if(board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1) {
+        nextTurn[1][1] = 1;
+    }
+
+    if(board[0][2] === 1 && board[1][1] === 1 && board[2][0] === 1) {
+        nextTurn[1][1] = 1;
+    }
+
+  return checkAllRows(board, nextTurn);
 };
 
-export const setupBoard = () => {
-  return [[0,0,0],
-          [0,0,0],
-          [0,0,0]];
+export const setupBoard = (rows, columns) => {
+
+    const board = [];
+
+    for (let i = 0; i < rows; i++){
+        const row = []
+        for (let j = 0; j < columns; j++){
+            row.push(0)
+        }
+        board.push(row)
+    }
+    console.log(board)
+  return board;
 };
+
+const wholeRowAlive = (row) => {
+    return row[0] === 1 && row[1] === 1 && row[2] === 1
+}
+
+const checkAllRows = (board, nextTurn) => {
+    const newNextTurn = nextTurn;
+    for (let rowIndex in board){
+        if (wholeRowAlive(board[rowIndex])) {
+            newNextTurn[rowIndex][1] = 1;
+       }
+    }
+    return newNextTurn;
+}
+
--- test/game.test.js
+++ test/game.test.js
@@ -2,22 +2,30 @@ import { setupBoard, progressGame } from "../src/game";
 
 describe ("Board setup", () => {    
     it("should setup empty board of 3x3", () => {
-        expect(setupBoard()).toEqual([[0,0,0],
+        expect(setupBoard(3, 3)).toEqual([[0,0,0],
                                       [0,0,0],
                                       [0,0,0]]);
+    });
+
+    it("should setup empty board of 5x5", () => {
+        expect(setupBoard(5, 5)).toEqual([[0,0,0,0,0],
+                                      [0,0,0,0,0],
+                                      [0,0,0,0,0],
+                                      [0,0,0,0,0],
+                                      [0,0,0,0,0]]);
     });
 })
 
 describe("Progress game", () => {
-    it("should return an empty board when passed an empty board", () => { 
-        const board = setupBoard();
+    it("should return an empty 3x3 board when passed an empty board", () => { 
+        const board = setupBoard(3, 3);
 
         expect(progressGame(board)).toEqual([[0,0,0],
                                              [0,0,0],
                                              [0,0,0]]);
     });
 
-    it("should return an empty board when passed a board with one live cell", () => {
+    it("should return an empty 3x3 board when passed a board with one live cell", () => {
         const board = [[0,0,0],
                        [0,1,0],
                        [0,0,0]];
@@ -27,7 +35,7 @@ describe("Progress game", () => {
                                              [0,0,0]]);
     });
 
-    it("should return an empty board when passed a board with two neighbouring live cells", () => {
+    it("should return an empty 3x3 board when passed a board with two neighbouring live cells in the column", () => {
         const board = [[0,0,0],
                        [0,1,0],
                        [0,1,0]];
@@ -37,10 +45,70 @@ describe("Progress game", () => {
                                              [0,0,0]]);
     });
 
-    it("should return a board with a single live cell when passed a board with three neighbouring live cells", () => { // fiona will be happy
+    it("should return an empty 3x3 board when passed a board with two neighbouring live cells on the same row", () => {
+        const board = [[0,0,0],
+                       [0,0,0],
+                       [0,1,1]];
+
+        expect(progressGame(board)).toEqual([[0,0,0],
+                                             [0,0,0],
+                                             [0,0,0]]);
+    });
+
+    it("should return a 3x3 board with a single live cell when passed a board with three neighbouring live cells on the top row", () => {
+        const board = [[1,1,1],
+                       [0,0,0],
+                       [0,0,0]];
+
+        expect(progressGame(board)).toEqual([[0,1,0],
+                                             [0,0,0],
+                                             [0,0,0]]);
+    });
+
+    it("should return a 3x3 board with a single live cell when passed a board with three neighbouring live cells on the middle row", () => {
+        const board = [[0,0,0],
+                       [1,1,1],
+                       [0,0,0]];
+
+        expect(progressGame(board)).toEqual([[0,0,0],
+                                             [0,1,0],
+                                             [0,0,0]]);
+    });
+
+    it("should return a 3x3 board with a single live cell when passed a board with three neighbouring live cells in the middle column", () => {
         const board = [[0,1,0],
                        [0,1,0],
                        [0,1,0]];
+
+        expect(progressGame(board)).toEqual([[0,0,0],
+                                             [0,1,0],
+                                             [0,0,0]]);
+    });
+
+    it("should return a 3x3 board with a single live cell when passed a board with three neighbouring live cells in the left column", () => {
+        const board = [[1,0,0],
+                       [1,0,0],
+                       [1,0,0]];
+
+        expect(progressGame(board)).toEqual([[0,0,0],
+                                             [1,0,0],
+                                             [0,0,0]]);
+    });
+
+    it("should return a 3x3 board with a single live cell when passed a board with three neighbouring live cells in a diagonal line", () => {
+        const board = [[1,0,0],
+                       [0,1,0],
+                       [0,0,1]];
+
+        expect(progressGame(board)).toEqual([[0,0,0],
+                                             [0,1,0],
+                                             [0,0,0]]);
+    });
+
+    it("should return a 3x3 board with a single live cell when passed a board with three neighbouring live cells in a different diagonal line", () => {
+        const board = [[0,0,1],
+                       [0,1,0],
+                       [1,0,0]];
 
         expect(progressGame(board)).toEqual([[0,0,0],
                                              [0,1,0],
